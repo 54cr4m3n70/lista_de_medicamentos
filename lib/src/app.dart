d3145e07912d952f5640dbe2af8f9bcecd93195a
Edilson Jardim Filho <edilsonj.filho@gmail.com>
2022-06-27 22:00:55 -0300

    chore: Adcionado estrutura da feature
    by: (54cr4m3n70) Edilson Jardim Filho <edilsonj.filho@gmail.com>

---
+++ lib/src/app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Medicamentos',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Medicamentos'),
+      ),
+      body: const Center(
+        child: Text('Dados dos Medicamentos'),
+      ),
+    );
+  }
+}
---
+++ lib/src/features/medicamentos/domain/entities/medicamento.dart
@@ -0,0 +1,10 @@
+class Medicamento {
+  int id;
+  String nome;
+  String descricao;
+  Medicamento({
+    required this.id,
+    required this.nome,
+    required this.descricao,
+  });
+}
