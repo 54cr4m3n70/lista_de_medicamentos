96109e83b6531db914b842a1e3198c55546fa13c
Edilson Jardim Filho <edilsonj.filho@gmail.com>
2022-08-09 21:49:07 -0300

    chore: criado createTables e save Medicamentos
    by: (54cr4m3n70) Edilson Jardim Filho <edilsonj.filho@gmail.com>

--- lib/src/features/medicamentos/data/repository_medicamentos.dart
+++ lib/src/features/medicamentos/data/repository_medicamentos.dart
@@ -1,48 +1,47 @@
 import 'package:lista_de_medicamentos/src/features/medicamentos/domain/entities/medicamento.dart';
+import 'package:lista_de_medicamentos/src/features/medicamentos/domain/repository/medicamento_repository.dart';
 import 'package:sqflite/sqflite.dart';
 
-import '../domain/repository/medicamento_repository.dart';
-
-class RepositoryMedicamentos implements IMedicamentoRepository {
-  late Database db;
-
-  Future open(String path) async {
-    db = await openDatabase(path, version: 1,
-        onCreate: (Database db, int version) async {
-      await db.execute(
-        '''
+class MedicamentosRepository implements IMedicamentoRepository {
+  static Future<void> createTables(Database db) async {
+    await db.execute('''
           CREATE table IF NOT EXISTS medicamentos(
             id INTERGER PRIMARY KEY autoincrement, 
             nome TEXT not null, 
             descricao INTERGER not null)
-        ''',
-      );
-    });
+          ''');
   }
 
-  @override
-  Future<List<Medicamento>> getAllMedicamentos() async {
-    final List<Map<String, dynamic>> maps = await db.query('medicamentos');
-
-    return List.generate(
-      maps.length,
-      (i) {
-        return Medicamento(
-          id: maps[i]['id'],
-          descricao: maps[i]['descricao'],
-          nome: maps[i]['nome'],
-        );
+  static Future<Database> db() async {
+    return openDatabase(
+      'medicamentos.db',
+      version: 1,
+      onCreate: (Database database, int version) async {
+        await createTables(database);
       },
     );
   }
 
   @override
-  Future<void> remove(int id) async {
-    await db.delete('medicamentos', where: '$id = ?', whereArgs: [id]);
+  Future<List<Medicamento>> getAllMedicamentos() {
+    // TODO: implement getAllMedicamentos
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> remove(int id) {
+    // TODO: implement remove
+    throw UnimplementedError();
   }
 
   @override
   Future<void> save(Medicamento item) async {
-    await db.insert('medicamentos', item.toMap());
+    final db = await MedicamentosRepository.db();
+    final data = item.toMap();
+    await db.insert(
+      'medicamentos',
+      data,
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
   }
 }
--- test/src/features/medicamentos/data/repository_medicamentos_test.dart
+++ test/src/features/medicamentos/data/repository_medicamentos_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:lista_de_medicamentos/src/features/medicamentos/domain/entities/medicamento.dart';
 import 'package:lista_de_medicamentos/src/features/medicamentos/domain/repository/medicamento_repository.dart';
 import 'package:lista_de_medicamentos/src/features/medicamentos/domain/usecases/medicamento_controller.dart';
 import 'package:mockito/mockito.dart';
@@ -6,13 +6,12 @@ import 'package:mockito/mockito.dart';
 class MockMedicamentos extends Mock implements IMedicamentoRepository {}
 
 void main() {
+  final medicamento = Medicamento(
+    id: 1,
+    nome: 'Dorflex',
+    descricao: 'Dor de Cabeça',
+  );
+
   MedicamentoController medicamentoController;
   MockMedicamentos mockMedicamentos;
-
-  setUp(() {
-    mockMedicamentos = MockMedicamentos();
-    medicamentoController = MedicamentoController(
-      medicamentoRepository: mockMedicamentos,
-    );
-  });
 }
